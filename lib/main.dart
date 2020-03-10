044f472f702d94c8122a525bdb986116eff647f4
cxb068396 <18368843089@163.com>
2020-03-10 14:54:28 +0800

    new project

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:waring_demo/views/history/history.dart';
+import 'package:waring_demo/views/home/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Widget child;
+
+  MyApp({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: '馨安家园',
+        theme: ThemeData(primaryColor: Colors.green),
+        home: MyStatckpage());
+  }
+}
+
+class MyStatckpage extends StatefulWidget {
+  final Widget child;
+
+  MyStatckpage({Key key, this.child}) : super(key: key);
+
+  _MyStatckpageState createState() => _MyStatckpageState();
+}
+
+class _MyStatckpageState extends State<MyStatckpage> {
+  var _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.green,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.history), title: Text('记录'))
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+        body: IndexedStack(
+          index: _currentIndex,
+          children: <Widget>[Home(), History()],
+        ));
+  }
+}
---
+++ lib/views/home/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:waring_demo/models/home_model.dart';
+import 'package:waring_demo/network/http_request.dart';
+
+import 'home_child/home_child.dart';
+
+class Home extends StatelessWidget {
+  final Widget child;
+
+  Home({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('首页'),
+        ),
+        body: HomeBody());
+  }
+}
+
+class HomeBody extends StatefulWidget {
+  final Widget child;
+
+  HomeBody({Key key, this.child}) : super(key: key);
+
+  _HomeBodyState createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  List<HomeModel> userList = [];
+
+  get index => null;
+  @override
+  void initState() {
+    super.initState();
+    HttpRequest.request('http://47.97.251.68:3000/call/activeCall').then((res) {
+      print(res.data);
+      List<HomeModel> users = [];
+      for (var user in res.data) {
+        users.add(HomeModel.fromJson(user));
+      }
+      setState(() {
+        this.userList = users;
+      });
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+        child: ListView.builder(
+      itemCount: userList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return HomeListItem(userList[index]);
+      },
+    ));
+  }
+}
